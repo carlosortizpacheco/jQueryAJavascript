16663884c64ee095395618b74f774e982ee32357
caortizpacheco <ca.ortiz.pacheco@gmail.com>
2020-10-19 09:33:23 -0500

    terminado el curso

--- src/js/home.js
+++ src/js/home.js
@@ -60,12 +60,16 @@ const getUser = new Promise( function(todoBien,todoMal) {
   async function getData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    return data;
+
+    if(data.data.movie_count > 0) {
+      return data
+    }
+    throw new Error ("no se encontro ningun resultado")
   }
 
-  function videoItemTemplate(movie){
+  function videoItemTemplate(movie,category){
     return (
-      `<div class="primaryPlaylistItem">
+      `<div class="primaryPlaylistItem" data-id='${movie.id}' data-category=${category}>
         <div class="primaryPlaylistItem-image">
           <img src="${movie.medium_cover_image}">
         </div>
@@ -83,21 +87,30 @@ const getUser = new Promise( function(todoBien,todoMal) {
   }
 
   function addEventClick($element) {
-    $element.addEventListener('click',function(){
-      showModal()
-    })
+    $element.addEventListener('click',showModal($element))
   }
 
-  const actionList = await getData('https://yts.mx/api/v2/list_movies.json?genre=action')
-  const terrorList = await getData('https://yts.mx/api/v2/list_movies.json?genre=terror')
-  const dramaList = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama')
-  const animationList = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation')
+  const actionList = await cacheExist('action')
+  const dramaList = await cacheExist('drama')
+  const animationList = await cacheExist('animation')
   
   const $actionContainer = document.querySelector('#action')
   const $dramaContainer = document.querySelector('#drama')
   const $animationContainer = document.querySelector('#animation')
   
-  const $featuringContainer = document.getElementById('featuring-container')
+  async function cacheExist(category) {
+    const listName = `${category}List`
+    const cacheList = window.localStorage.getItem(listName)
+    if(cacheExist) {
+      return JSON.parse(cacheList)
+    }
+    const { data : { movies: data } } =  await getData(`https://yts.mx/api/v2/list_movies.json?genre=${category}`)
+    window.localStorage.setItem(listName,JSON.stringify(data))
+
+    return data
+  }
+
+  const $featuringContainer = document.getElementById('featuring')
   const $form = document.getElementById('form')
   const $home = document.getElementById('home')
 
@@ -109,28 +122,47 @@ const getUser = new Promise( function(todoBien,todoMal) {
   const $modalImage = $modal.querySelector('img')
   const $modalDescription = $modal.querySelector('p')
 
-  function renderMoviesList(list,$container) {
+  function renderMoviesList(list,$container,category) {
     $container.children[0].remove()
     list.forEach( movie => {
-      const HTMLString = videoItemTemplate(movie)
+      const HTMLString = videoItemTemplate(movie,category)
       const movieElement = createTemplate(HTMLString)
       $container.append(movieElement)
-      
+      const image = movieElement.querySelector('img')
+      image.addEventListener('load', (event) => {
+        event.target.classList.add('fadeIn')
+      })
       addEventClick(movieElement)
     })
   }
  
-  renderMoviesList(actionList.data.movies,$actionContainer)
-  renderMoviesList(dramaList.data.movies,$dramaContainer)
-  renderMoviesList(animationList.data.movies,$animationContainer)
+  renderMoviesList(actionList,$actionContainer,'action')
+  renderMoviesList(dramaList,$dramaContainer,'drama')
+  renderMoviesList(animationList,$animationContainer,'animation')
 
   function setAttributes($element,attributes) {
     for(const attribute in attributes) {
-      $element.setAttributes(attribute, attributes[attribute])
+      $element.setAttribute(attribute, attributes[attribute])
     }
   }
 
-  $form.addEventListener('submit', function(event) {
+  function featuringTemplate(pelicula) {
+    return (
+      `
+      <div class="featuring">
+        <div class="featuring-image">
+          <img src="${pelicula.medium_cover_image}" width="70" height="100" alt="">
+        </div>
+        <div class="featuring-content">
+          <p class="featuring-title">Pelicula encontrada</p>
+          <p class="featuring-album">${pelicula.title}</p>
+        </div>
+      </div>
+      `
+    )
+  }
+
+  $form.addEventListener('submit', async function(event) {
     event.preventDefault()
     $home.classList.add('search-active')
     const $loader = document.createElement('img')
@@ -139,20 +171,57 @@ const getUser = new Promise( function(todoBien,todoMal) {
       height: 50,
       width: 50
     })
+    $featuringContainer.append($loader)
+    const data = new FormData($form)
+
+    try {
+      const { data: { movies: pelis } } = await getData(`https://yts.mx/api/v2/list_movies.json?limit=1&query_term=${data.get('name')}`)
+      const HTMLString = featuringTemplate(pelis[0])
+      $featuringContainer.innerHTML = HTMLString
+    } catch (error) {
+      alert(error.message)
+      $loader.remove()
+      $home.classList.remove('search-active')
+    }
+
+
   })
 
-  function showModal() {
-    $overlay.classList.add('active')
-    $modal.style.animation = 'modalIn .8s forwards'
+  function findById (list,id) {
+    return list.find(movie => movie.id === parseInt(id,10))
   }
 
+  function findMovie(id,category) {
+    switch(category) {
+      case 'action': {
+        return findById(actionList,id)
+      }
+      case 'drama': {
+        return findById(dramaList,id)
+      }
+      default: {
+        return findById(animationList,id)
+      }
+    }
+  }
+
+  function showModal($element) {
+    $overlay.classList.add('active')
+    $modal.style.animation = 'modalIn .8s forwards'
+    const id = $element.dataset.id
+    const category = $element.dataset.category
+    const data = findMovie (id,category)
+
+    $modalTitle.textContent = data.title
+    $modalImage.setAttribute('src',data.medium_cover_image)
+    $modalDescription.textContent = data.summary
+  }
+  
   $hideModal.addEventListener('click',hideModal)
+  
   function hideModal() {
     $overlay.classList.remove('active')
     $modal.style.animation = 'modalOut .8s forwards'
   }
-
-
-  
 
 })()
